3b8a6fa5c994a626258870f44cdc0612dabb01a5
Oshota501 <kakidesu1111@gmail.com>
2025-12-13 18:14:58 +0900

    feat : prefabをクローン、

---
+++ Assets/Scripts/ObjectCloner.cs
@@ -0,0 +1,72 @@
+using UnityEngine;
+
+public class ObjectCloner : MonoBehaviour
+{
+    public float Sensibility = 0.005f;
+    // public GameObject box ;
+    private Rigidbody rb ;
+    private bool isFalling = false ;
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.rb = GetComponent<Rigidbody>();
+        this.rb.useGravity = false ;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!isFalling){
+            KeyEvents();
+        }
+    }
+    private void KeyEvents () {
+        if(Input.GetKey(KeyCode.W))
+        {
+            this.transform.position += new Vector3(0f,0f,Sensibility);
+        }
+        if(Input.GetKey(KeyCode.S))
+        {
+            this.transform.position -= new Vector3(0f,0f,Sensibility);
+        }
+        if(Input.GetKey(KeyCode.A))
+        {
+            this.transform.position += new Vector3(Sensibility,0f,0f);
+        }
+        if(Input.GetKey(KeyCode.D))
+        {
+            this.transform.position -= new Vector3(Sensibility,0f,0f);
+        }
+        if(Input.GetKey(KeyCode.C)){
+            this.isFalling = true ;
+            this.rb.useGravity = true ;
+            Invoke(nameof(Clone), 0.7f);
+        }
+    }
+    void OnCollisionEnter(Collision collision){
+        if(collision.gameObject.name == this.gameObject.name){
+            Destroy(this.gameObject) ;
+            Destroy(collision.gameObject) ;
+        }
+    }
+    private void Clone() {
+        ClonerSetting.Clone();
+    }
+    // private bool IsInArea_X ( Vector3 pos)
+    // {
+    //     float posx = pos.x - box.transform.position.x ;
+    //     return (
+    //         posx < 3.0f &&
+    //         posx > 0.0f
+    //     ) ;
+    // }
+
+    // private bool IsInArea_Y(Vector3 pos)
+    // {
+    //     float posy = pos.y - box.transform.position.y;
+    //     return (
+    //         posy > 0.0f && 
+    //         posy < 3.0f
+    //     );
+    // }
+}
